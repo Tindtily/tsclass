09da00f83ff1d73147e1eb48b6dbbb77c638a1a0
zhaoxhai <283893159@qq.com>
2013-09-08 12:53:39 +0800

    up

--- aas.js
+++ aas.js
@@ -79,7 +79,7 @@ if (document.location.href.indexOf('student') > 0) {
     fuckZy()
 } else if (document.location.href.indexOf('courseAction') > 0) {
     
-            initUI()
+            initUI();
    )
 } else {
     alert('\u62dc\u6258\uff01\u60a8\u786e\u5b9a\u60a8\u5728\u89c6\u9891\u64ad\u653e\u9875\u9762\u6216\u4f5c\u4e1a\u9875\u9762\uff1f')
